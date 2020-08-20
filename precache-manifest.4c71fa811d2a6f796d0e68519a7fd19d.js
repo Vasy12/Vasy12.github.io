77abb6293ade0b34b29018b07ce0d8705e712aba
User Name <artur.it2914@gmail.com>
2020-08-20 15:07:54 +0300

    Build

---
+++ precache-manifest.4c71fa811d2a6f796d0e68519a7fd19d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abeb46192e4ba9a14390d3a1fa8243e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c38fc723639e9a28886",
+    "url": "/static/css/main.5a65c742.chunk.css"
+  },
+  {
+    "revision": "0c32823ea195b9775965",
+    "url": "/static/js/2.726dde96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.726dde96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c38fc723639e9a28886",
+    "url": "/static/js/main.e15065ef.chunk.js"
+  },
+  {
+    "revision": "303ff6c8f1a278a2ac86",
+    "url": "/static/js/runtime-main.1fca8c80.js"
+  }
+]);
